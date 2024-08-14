8507ea1c3be69323f84293e4b035b7fb9146f442
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-14 13:42:15 +0200

    Add inputs and selects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CurrentRateComponent} from "./current-rate/current-rate.component";
+import {CurrencyConversionComponent} from "./currency-conversion/currency-conversion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurrentRateComponent],
+  imports: [RouterOutlet, CurrentRateComponent, CurrencyConversionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
