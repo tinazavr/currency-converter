1c330a5a3d169f6600014ba04c420b97c48185ba
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-15 21:21:30 +0200

    Set general styles for page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CurrentRateComponent} from "./current-rate/current-rate.component";
 import {CurrencyConversionComponent} from "./currency-conversion/currency-conversion.component";
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CurrentRateComponent, CurrencyConversionComponent],
+  providers: [{ provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
