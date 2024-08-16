2cf9e08748a271c52684146ccea7596fdb5c22a5
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-16 13:48:50 +0200

    Rewrite service for every used currency

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {firstValueFrom, forkJoin} from "rxjs";
 
+const apiLink = 'https://v6.exchangerate-api.com/v6/dde4fa9f19f23331bdb99d19/latest'
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +11,18 @@ export class CurrencyService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getExchangeRates() {
-    return this.httpClient.get <any
-    >(`https://v6.exchangerate-api.com/v6/dde4fa9f19f23331bdb99d19/latest/UAH`)
+  async getExchangeRates() {
+     const [uah, usd, eur, pln] = await firstValueFrom(forkJoin([
+       this.httpClient.get <any>(apiLink + `/UAH`),
+       this.httpClient.get <any>(apiLink + `/USD`),
+       this.httpClient.get <any>(apiLink + `/EUR`),
+       this.httpClient.get <any>(apiLink + `/PLN`)
+       ]
+     ))
+    return {uah: uah.conversion_rates,
+      usd: usd.conversion_rates,
+      eur: eur.conversion_rates,
+      pln: pln.conversion_rates,
+    }
   }
 }
