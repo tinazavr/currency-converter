2cf15f70c3988f95b7c6b70f72afad0bb12346eb
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-16 21:11:34 +0200

    Use api data in rate table

--- src/app/current-rate/current-rate.component.ts
+++ src/app/current-rate/current-rate.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatTableModule} from "@angular/material/table";
 import {CurrencyRate} from "../currency-rate";
 import {CurrencyService} from "../currency.service";
@@ -11,18 +11,21 @@ import {CurrencyService} from "../currency.service";
   styleUrl: './current-rate.component.scss'
 })
 export class CurrentRateComponent {
-  currencyData:CurrencyRate[]=[];
-  constructor(private currencyService: CurrencyService) {
+  displayedColumns: string[] = ['what-to-do', 'usd', 'eur', 'pln'];
+  dataSource: CurrencyRate[] = [];
 
-    this.currencyService.getExchangeRates().subscribe((data)=>{
-      console.log(data.conversion_rates);
-      this.dataSource = [
-        {whatToDo: "Buy", usd: Math.round(1/data.conversion_rates.USD), eur: Math.round(1/data.conversion_rates.EUR), pln: Math.round(1/data.conversion_rates.PLN)},
-        {whatToDo: "Sell", usd: Math.round(1/data.conversion_rates.USD * 1.1), eur: Math.round(1/data.conversion_rates.EUR * 1.1), pln: Math.round(1/data.conversion_rates.PLN * 1.1)},
-      ];
-    })
+  constructor(private currencyService: CurrencyService) {
   }
 
-  displayedColumns: string[] = ['what-to-do', 'usd', 'eur', 'pln'];
-  dataSource:CurrencyRate[] = [];
+  async ngOnInit() {
+    const data = await this.currencyService.getExchangeRates()
+    this.dataSource = [{
+      whatToDo: "Buy", usd: data.usd.UAH, eur: data.eur.UAH, pln: data.pln.UAH
+    }, {
+      whatToDo: "Sell",
+      usd: Math.round(data.usd.UAH * 1.01 * 100) / 100,
+      eur: Math.round(data.eur.UAH * 1.01 * 100) / 100,
+      pln: Math.round(data.pln.UAH * 1.01 * 100) / 100
+    },]
+  }
 }
