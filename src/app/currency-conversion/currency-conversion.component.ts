0ae6753a19b2f464d7dd2ce82d433de773d3a7da
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-26 13:30:19 +0200

    Add number validation and use reactive forms

--- src/app/currency-conversion/currency-conversion.component.ts
+++ src/app/currency-conversion/currency-conversion.component.ts
@@ -1,25 +1,35 @@
 import {Component, Input} from '@angular/core';
 import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatSelectModule} from "@angular/material/select";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {NgIf} from "@angular/common";
+import Validation from "../shared/validation";
 
 type CurrencyOption = "usd" | "pln" | "eur" | "uah";
 @Component({
   selector: 'app-currency-conversion',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, NgIf],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, NgIf, ReactiveFormsModule],
   templateUrl: './currency-conversion.component.html',
   styleUrl: './currency-conversion.component.scss'
 })
 export class CurrencyConversionComponent {
+  valid: Validation = new Validation()
+
   @Input() exchangeRate: any;
   convertFromValue :number | undefined;
   convertToValue :number | undefined;
   currencyFrom : CurrencyOption = "usd";
   currencyTo : CurrencyOption = "uah";
   currency: CurrencyOption[] = ['usd', 'eur','pln', 'uah'];
+
+  form = new FormGroup({
+    enter: new FormControl('', {validators: [Validators.required, this.valid.numberValidator]}),
+    convertFrom: new FormControl(this.currency),
+    result: new FormControl('', {validators: [Validators.required]}),
+    convertTo: new FormControl(this.currency),
+  })
 
   onConvertFromChange() {
     if(typeof this.convertFromValue !== "undefined") {
