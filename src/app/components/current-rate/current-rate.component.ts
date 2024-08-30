a825d1241ff62b00502985269e4e0deac690b4c0
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-08-30 10:40:35 +0200

    Replace the code that was duplicated

--- src/app/components/current-rate/current-rate.component.ts
+++ src/app/components/current-rate/current-rate.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {MatTableModule} from "@angular/material/table";
 import {CurrencyRate} from "../../types/currency-rate";
 import {ExchangeRateMap} from "../../types/exchange-rate-map";
+import {CurrencyOption} from "../../types/currency-option";
 
 @Component({
   selector: 'app-current-rate',
@@ -10,19 +11,26 @@ import {ExchangeRateMap} from "../../types/exchange-rate-map";
   templateUrl: './current-rate.component.html',
   styleUrl: './current-rate.component.scss'
 })
-export class CurrentRateComponent implements OnInit{
+export class CurrentRateComponent implements OnInit {
   @Input() exchangeRate !: ExchangeRateMap;
   displayedColumns: string[] = ['what-to-do', 'usd', 'eur', 'pln'];
   dataSource: CurrencyRate[] = [];
 
   async ngOnInit() {
     this.dataSource = [{
-      whatToDo: "Buy", usd: this.exchangeRate.usd['UAH'], eur: this.exchangeRate.eur['UAH'], pln: this.exchangeRate.pln['UAH']
+      whatToDo: "Buy",
+      usd: this.exchangeRate.usd['UAH'],
+      eur: this.exchangeRate.eur['UAH'],
+      pln: this.exchangeRate.pln['UAH']
     }, {
       whatToDo: "Sell",
-      usd: Math.round(this.exchangeRate.usd['UAH'] * 1.01 * 100) / 100,
-      eur: Math.round(this.exchangeRate.eur['UAH'] * 1.01 * 100) / 100,
-      pln: Math.round(this.exchangeRate.pln['UAH'] * 1.01 * 100) / 100
+      usd: this.calculateSellRate('usd'),
+      eur: this.calculateSellRate('eur'),
+      pln: this.calculateSellRate('pln')
     },]
+  }
+
+  private calculateSellRate(currency: CurrencyOption) {
+    return (Math.round(this.exchangeRate[currency]['UAH'] * 1.01 * 100) / 100)
   }
 }
